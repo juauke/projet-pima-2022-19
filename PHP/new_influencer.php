5cfdd9a267425daf4fd46c76c77efcdd9cb4110c
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-10-07 18:55:07 +0200

    Tests ok pour la fonction create_new_influencer

--- PHP/new_influencer.php
+++ PHP/new_influencer.php
@@ -4,14 +4,14 @@
      * @param int $id_user ID of the user.
      */
     function create_new_influencer($lastname, $firstname, $genre, $pseudo, $id_user){
-        $data_location = "../data/influenceurs_suivis.json";
+        $data_location = "./data/influenceurs_suivis.json";
         $file=fopen($data_location, "r");
         $data = fread($file, filesize($data_location));
         fclose($file);
 
         $data = json_decode($data, true);
-        $size = sizeof($data[0]);
-        $data[0][$size] = array("nom" => $lastname, "prenom" => $firstname, "genre" => $genre, "pseudo" => $pseudo, "id" => $size, "suivi_par" => array($id_user));
+        $size = sizeof($data["influenceurs_suivis"]);
+        $data["influenceurs_suivis"][$size] = array("nom" => $lastname, "prenom" => $firstname, "genre" => $genre, "pseudo" => $pseudo, "id" => $size, "suivis_par" => array($id_user));
 
         $file=fopen($data_location, "w");
         fwrite($file, json_encode($data));
