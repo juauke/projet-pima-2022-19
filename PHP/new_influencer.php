fb33b3d372f4310f93f71a9efb6b4737cfcac638
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-10-07 18:09:07 +0200

    Fonction d'ajout d'influenceur ok, en attente de test

--- PHP/new_influencer.php
+++ PHP/new_influencer.php
@@ -1,5 +1,20 @@
 <?php
-    function create_new_influencer($name){
-        
+    /**
+     * @param string $name Name of the influencer.
+     * @param int $id_user ID of the user.
+     */
+    function create_new_influencer($lastname, $firstname, $genre, $pseudo, $id_user){
+        $data_location = "../data/influenceurs_suivis.json";
+        $file=fopen($data_location, "r");
+        $data = fread($file, filesize($data_location));
+        fclose($file);
+
+        $data = json_decode($data, true);
+        $size = sizeof($data[1]);
+        $data[1][$size] = array("nom" => $lastname, "prenom" => $firstname, "genre" => $genre, "pseudo" => $pseudo, "id" => $size, "suivi_par" => array($id_user));
+
+        $file=fopen($data_location, "w");
+        fwrite($file, json_encode($data));
+        fclose($file);
     }
 ?>
