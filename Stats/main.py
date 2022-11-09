a659fbd0963b33eb179ea4f06f25c01a38541e47
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-11-09 15:16:52 +0100

    Added logging entries for database

--- Stats/main.py
+++ Stats/main.py
@@ -5,13 +5,19 @@ import mariadb as db
 
 import sys
 
+from datetime import date
+
 def queryPassword():
     f = open("/passwords/pw.ini", "r")
     passwd = f.readline()
     f.close()
     return passwd.split(':')
 
+logfile = open("/var/log/stats/Stats.log", "a")
+print(f"{date.today()}\n", file=logfile)
+
 try:
+    print(f'Connecting to database...', file=logfile)
     id = queryPassword()
     conn = db.connect(
         user=id[0].strip('\n'),
@@ -19,8 +25,10 @@ try:
         host="10.1.0.4",
         database="influenceurs"
     )
-    
+    print(f'Connected to database', file=logfile)
     conn.close()
+    print(f'Disconnected from database', file=logfile)
 except db.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
+    print(f"Error connecting to MariaDB Platform: {e}", file=logfile)
+    logfile.close()
     sys.exit(1)
