c802881d9038abc880c84fbc1dbfc19c5fda2716
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2022-11-16 15:25:56 +0100

    Working MySQL version of the register/login system

--- PHP/Gestion_Compte/welcome.php
+++ PHP/Gestion_Compte/welcome.php
@@ -2,7 +2,13 @@
 // Initialize the session
 session_start();
 
-header("location: ../index.html");
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+header("location: ../../build/index.html");
 exit;
 
 ?>
