117aa35362b66935f606e99beae11b32b17463f3
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2022-11-16 13:50:51 +0100

    Redirection to the main index.html (= main website) when logging in, should work but cannot test

--- PHP/Gestion_Compte/login.php
+++ PHP/Gestion_Compte/login.php
@@ -14,7 +14,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 <head>
     <meta charset="UTF-8">
     <title>Connexion</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
