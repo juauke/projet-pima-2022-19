c3d953cd9499b0c0f614522c9c5c49eecabc26eb
aurélien bourgerie <aurelien.bourgerie@etu.univ-orleans.fr>
2022-11-28 00:21:46 +0100

    reglage bugs

--- PHP/Gestion_Compte/reset-password.php
+++ PHP/Gestion_Compte/reset-password.php
@@ -1,7 +1,72 @@
 <?php
 // Initialize the session
 session_start();
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+  header("location: login.php");
+  exit;
+}
 
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  // Validate new password
+  if(empty(trim($_POST["new_password"]))){
+      $new_password_err = "Veuillez entrer le nouveau mot de passe.";
+  } elseif(strlen(trim($_POST["new_password"])) < 6){
+      $new_password_err = "Un mot de passe doit contenir au moins 6 caractères.";
+  } else{
+      $new_password = trim($_POST["new_password"]);
+  }
+
+  // Validate confirm password
+  if(empty(trim($_POST["confirm_password"]))){
+      $confirm_password_err = "Veuillez confirmer le mot de passe.";
+  } else{
+      $confirm_password = trim($_POST["confirm_password"]);
+      if(empty($new_password_err) && ($new_password != $confirm_password)){
+          $confirm_password_err = "Les mots de passe ne correspondent pas.";
+      }
+  }
+
+  // Check input errors before updating the database
+  if(empty($new_password_err) && empty($confirm_password_err)){
+      // Prepare an update statement
+      $sql = "UPDATE users SET password = ? WHERE id = ?";
+
+      if($stmt = mysqli_prepare($link, $sql)){
+          // Bind variables to the prepared statement as parameters
+          mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+
+          // Set parameters
+          $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+          $param_id = $_SESSION["id"];
+
+          // Attempt to execute the prepared statement
+          if(mysqli_stmt_execute($stmt)){
+              // Password updated successfully. Destroy the session, and redirect to login page
+              session_destroy();
+              header("location: login.php");
+              exit();
+          } else{
+              echo "Oups ! Quelque chose s'est mal passée. Merci de réessayer ultérieurement..";
+          }
+
+          // Close statement
+          mysqli_stmt_close($stmt);
+      }
+  }
+
+  // Close connection
+  mysqli_close($link);
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +74,10 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Réinitialisation du mot de passe</title>
-    <link rel="stylesheet" href="../../CSS/styles.css">
-    <script type="text/javascript" src="../../JS/jquery.min.js"></script>
-<script type="text/javascript" src="../../JS/particles.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="JS/particles.js"></script>
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
@@ -146,7 +212,7 @@ session_start();
 <div class="wrapper">
     <h2>Réinitialisation du mot de passe</h2>
     <p>Veuillez remplir ce formulaire pour réinitialiser votre mot de passe.</p>
-    <form action="login.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group">
             <label>Nouveau mot de passe</label>
             <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
--- PHP/Gestion_Compte/welcome.php
+++ PHP/Gestion_Compte/welcome.php
@@ -4,5 +4,30 @@ session_start();
 
 header("location: ../../index.html");
 exit;
+if (isset($_SESSION["loggedin"]))
+{
+$id = $_SESSION["loggedin"];
+}
+
+
+if (isset($_SESSION["id"]))
+{
+$id = $_SESSION["id"];
+}
+
+if (isset($_SESSION["username"]))
+{
+$username = $_SESSION["username"];
+}
+
+?>
+
+<script>
+
+sessionStorage.setItem("loggedin",<?php echo $loggedin; ?>);
+sessionStorage.setItem("username",<?php echo $username; ?>);
+sessionStorage.setItem("id",<?php echo $id; ?>);
+
+</script>
 
 ?>
